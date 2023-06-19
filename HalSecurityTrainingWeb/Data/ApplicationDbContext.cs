6820e47b41f33ec81e8114d71f9aa695fa271dd9
Swetha Chadalavada <Swetha Chadalavada@DESKTOP-3ICJG0Q>
2023-06-19 19:50:24 -0400

    Add project files.

---
+++ HalSecurityTrainingWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using HalSecurityTrainingWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HalSecurityTrainingWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ HalSecurityTrainingWeb/Migrations/20230619214424_AddStudenttoDatabase.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HalSecurityTrainingWeb.Migrations
+{
+    public partial class AddStudenttoDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DOB = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RegistrationDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
